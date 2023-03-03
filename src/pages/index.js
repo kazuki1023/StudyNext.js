b3c78a048b19e4a11be7ec315e72d3c4d997503f
kazuki1023 <k.keio12569@gmail.com>
2023-03-03 14:40:18 +0000

    リンク途中、これからviewpointについて調べる

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
+import Link from "next/link";
 export default function Home() {
-  return <h1>Hello World index.js</h1>;
+  return (
+  <div>
+    <ul>
+      <li>
+        <Link href="/about">about
+        </Link>
+      </li>
+    </ul>
+    <h1>Hello Next.js</h1>
+  </div>);
 }
 
